2c69bc222b631cd0840aaf38958b1a934fc8bd81
vpc <cuevasvp@gmail.com>
2020-05-19 12:52:26 +0800

    alternate solution

--- MaxSumCircularSubarray.py
+++ MaxSumCircularSubarray.py
@@ -47,19 +47,19 @@ def max_subarr_sum(arr):
     return maxsum
 
 
-def max_subarr_sum_circular(arr):
-    if not arr:
-        return 0
-    if all([e < 0 for e in arr]):  # all values are negative
-        return max(arr)
-
-    maxsum = max_subarr_sum(arr)  # kadane max subarray sum
-    arrsum = 0
-    for i in range(len(arr)):
-        arrsum += arr[i]
-        arr[i] = -arr[i]
-
-    return max(maxsum, arrsum + max_subarr_sum(arr))
+# def max_subarr_sum_circular(arr):
+#     if not arr:
+#         return 0
+#     if all([e < 0 for e in arr]):  # all values are negative
+#         return max(arr)
+#
+#     maxsum = max_subarr_sum(arr)  # kadane max subarray sum
+#     arrsum = 0
+#     for i in range(len(arr)):
+#         arrsum += arr[i]
+#         arr[i] = -arr[i]
+#
+#     return max(maxsum, arrsum + max_subarr_sum(arr))
 
 # def max_subarr_sum_circular_naive(arr):
 #     if not arr:
@@ -72,6 +72,29 @@ def max_subarr_sum_circular(arr):
 #         maxsum = max(maxsum, max_subarr_sum(arr[i:lenarr + i]))
 #     return maxsum
 
+def max_subarr_sum_circular(arr):
+    if all([e < 0 for e in arr]):  # all values are negative
+        return max(arr)
+    n = len(arr)
+
+    subarr_sum = 0
+    maxsum = -sys.maxsize
+    for e in arr:
+        subarr_sum = max(e, subarr_sum + e)
+        maxsum = max(maxsum, subarr_sum)
+
+    rightsum = arr[-1]
+    maxright = [0] * (n - 1) + [arr[-1]]
+    for i in range(n - 2, -1, -1):
+        rightsum += arr[i]
+        maxright[i] = max(rightsum, maxright[i + 1])
+
+    leftsum = 0
+    for i in range(n - 2):
+        leftsum += arr[i]
+        maxsum = max(maxsum, leftsum + maxright[i + 2])
+    return maxsum
+
 
 assert max_subarr_sum_circular([1, -2, 3, -2]) == 3
 assert max_subarr_sum_circular([5, -3, 5]) == 10
@@ -79,4 +102,4 @@ assert max_subarr_sum_circular([3, -1, 2, -1]) == 4
 assert max_subarr_sum_circular([3, -2, 2, -3]) == 3
 assert max_subarr_sum_circular([-2, -3, -1]) == -1
 
-print(max_subarr_sum_circular([1, -2, 3, -4, 5]))
+# print(max_subarr_sum_circular([1, -2, 3, -4, 5]))

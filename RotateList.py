32ed595becb94d99f944504aab574d4bad00779b
vpc <cuevasvp@gmail.com>
2020-05-25 12:49:57 +0800

    solutions for rotate left and right

--- RotateList.py
+++ RotateList.py
@@ -1,3 +1,24 @@
+# Given a linked list, rotate the list to the right by k places, where k is non-negative.
+#
+# Example 1:
+#
+# Input: 1->2->3->4->5->NULL, k = 2
+# Output: 4->5->1->2->3->NULL
+# Explanation:
+# rotate 1 steps to the right: 5->1->2->3->4->NULL
+# rotate 2 steps to the right: 4->5->1->2->3->NULL
+#
+# Example 2:
+#
+# Input: 0->1->2->NULL, k = 4
+# Output: 2->0->1->NULL
+# Explanation:
+# rotate 1 steps to the right: 2->0->1->NULL
+# rotate 2 steps to the right: 1->2->0->NULL
+# rotate 3 steps to the right: 0->1->2->NULL
+# rotate 4 steps to the right: 2->0->1->NULL
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -11,7 +32,23 @@ def print_list(curr):
     print('')
 
 
-def rotate_list(head, k):
+def rotate_right(head, k):
+    if head is None:
+        return None
+    if k == 0:
+        return head
+
+    l = length_list(head)
+    return rotate_left(head, l - k % l)
+
+
+def rotate_left(head, k):
+    if head is None:
+        return None
+    k = k % length_list(head)
+    if k == 0:
+        return head
+
     curr = head
     i = 1
     while curr.next:
@@ -26,20 +63,29 @@ def rotate_list(head, k):
     return new_head
 
 
+def length_list(head):
+    curr = head
+    l = 0
+    while curr:
+        l += 1
+        curr = curr.next
+    return l
+
+
 node1 = ListNode(1)
 node2 = ListNode(2)
 node3 = ListNode(3)
 node4 = ListNode(4)
 node5 = ListNode(5)
-node6 = ListNode(6)
-node7 = ListNode(7)
 
 node1.next = node2
 node2.next = node3
 node3.next = node4
 node4.next = node5
-node5.next = node6
-node6.next = node7
 
-print_list(node1)
-print_list(rotate_list(node1, 2))
+print_list(rotate_left(node1, 0))
+
+# print_list(rotate_left(node1, 1))
+print_list(rotate_right(None, 0))
+
+# print_list(rotate_left(node1, 5))

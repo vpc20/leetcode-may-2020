a7cab846f10c8f05adb7b9c7d2e96cfbbc25e285
vpc <cuevasvp@gmail.com>
2020-07-01 07:27:00 +0800

    alternate solution

--- TrieImplementation.py
+++ TrieImplementation.py
@@ -17,6 +17,45 @@
 # from collections import defaultdict
 
 
+class Node:
+    def __init__(self, char):
+        self.val = char
+        self.children = {}
+        self.end = False
+
+    def __repr__(self):
+        return f'Node({self.val} : {self.children.keys()})'
+
+
+class Trie:
+    def __init__(self):
+        self.root = Node(None)
+
+    def insert(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Node(c)
+            curr = curr.children[c]
+        curr.end = True
+
+    def search(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.end
+
+    def starts_with(self, prefix):
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+
+
 # class Node:
 #     def __init__(self, char):
 #         self.char = char
@@ -69,41 +108,59 @@
 #                 return node
 #         return None
 
-class Trie:
-    def __init__(self):
-        self.d = {}
-
-    def insert(self, word):
-        node = self.d
-        for c in word:
-            if c not in node:
-                node[c] = {}
-            node = node[c]
-        node['$'] = True
-
-    def search(self, word):
-        node = self.d
-        for c in word:
-            if c in node:
-                node = node[c]
-            else:
-                return False
-        return '$' in node
-
-    def startsWith(self, prefix):
-        node = self.d
-        for c in prefix:
-            if c in node:
-                node = node[c]
-            else:
-                return False
-        return True
+# class Trie:
+#     def __init__(self):
+#         self.d = {}
+#
+#     def insert(self, word):
+#         node = self.d
+#         for c in word:
+#             if c not in node:
+#                 node[c] = {}
+#             node = node[c]
+#         node['$'] = True
+#
+#     def search(self, word):
+#         node = self.d
+#         for c in word:
+#             if c in node:
+#                 node = node[c]
+#             else:
+#                 return False
+#         return '$' in node
+#
+#     def startsWith(self, prefix):
+#         node = self.d
+#         for c in prefix:
+#             if c in node:
+#                 node = node[c]
+#             else:
+#                 return False
+#         return True
 
 
 trie = Trie()
+
 trie.insert('apple')
 assert trie.search('apple') is True
 assert trie.search('app') is False
-assert trie.startsWith('app') is True
+assert trie.starts_with('app') is True
 trie.insert('app')
 assert trie.search('app') is True
+
+# ["Trie","insert",  "search", "search",  "search", "startsWith","startsWith","startsWith"]
+# [[],    ["hello"], ["hell"], ["helloa"],["hello"],["hell"],     ["helloa"],  ["hello"]]
+# trie.insert('hello')
+# print(trie.search('hell'))
+# print(trie.search('helloa'))
+# print(trie.search('hello'))
+# print(trie.starts_with('hell'))
+# print(trie.starts_with('hello'))
+
+# trie.insert('apple')
+# trie.insert('app')
+# print(trie.search('app'))
+# ["Trie", "insert",  "search",  "search", "startsWith", "insert", "search"]
+# [[],     ["apple"], ["apple"], ["app"] , ["app"],      ["app"] , ["app"]]
+
+

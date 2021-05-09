360582d1dba7cab23a9fa03d04d693a522798a96
vpc <cuevasvp@gmail.com>
2021-05-10 05:20:04 +0800

    refactor

--- CourseSchedule.py
+++ CourseSchedule.py
@@ -29,11 +29,11 @@ BLACK = 2  # finished
 
 
 def can_finish(n, preqs):
-    def dfs(g, u):
+    def dfs(u):
         colors[u] = GRAY
         for v in g[u]:
             if colors[v] == WHITE:
-                if not dfs(g, v):
+                if not dfs(v):
                     return False
             elif colors[v] == GRAY:
                 return False
@@ -43,9 +43,9 @@ def can_finish(n, preqs):
     if not preqs:
         return True
     g = defaultdict(list)  # graph for prerequisites
-    for v2, v1 in preqs:  # directed graph
-        g[v1].append(v2)
-    print(g)
+    for v, u in preqs:  # directed graph
+        g[u].append(v)
+    # print(g)
 
     colors = {}  # vertex colors
     for u in range(n):
@@ -53,14 +53,55 @@ def can_finish(n, preqs):
 
     for u in list(g):
         if colors[u] == WHITE:
-            if not dfs(g, u):
+            if not dfs(u):
                 return False
     return True
 
 
-print(can_finish(1, []))
-print(can_finish(2, [[1, 0]]))
-print(can_finish(3, [[2, 1], [1, 0]]))
-print(can_finish(4, [[3, 2], [2, 1], [1, 0]]))
-print(can_finish(4, [[1, 2], [3, 2], [2, 1], [1, 0]]))
-print(can_finish(5, [[1, 0], [2, 1], [3, 4], [4, 3]]))
+# def can_finish(n, preqs):
+#     def dfs(u):
+#         visited.add(u)
+#         cycles.add(u)
+#         for v in g[u]:
+#             if v not in visited:
+#                 if not dfs(v):
+#                     return False
+#             elif v in cycles:
+#                 return False
+#         cycles.remove(u)
+#         return True
+#
+#     if not preqs:
+#         return True
+#     g = defaultdict(list)  # graph for prerequisites
+#     for i in range(n):
+#         g[i] = []
+#     for v, u in preqs:  # directed graph
+#         g[u].append(v)
+#     print(g)
+#
+#     visited = set()
+#     cycles = set()
+#     for u in list(g):
+#         if u not in visited:
+#             if not dfs(u):
+#                 return False
+#     return True
+
+
+# print(can_finish(1, []))
+# print(can_finish(2, [[1, 0]]))
+# print(can_finish(3, [[2, 1], [1, 0]]))
+# print(can_finish(4, [[3, 2], [2, 1], [1, 0]]))
+# print(can_finish(4, [[1, 2], [3, 2], [2, 1], [1, 0]]))
+# print(can_finish(5, [[1, 0], [2, 1], [3, 4], [4, 3]]))
+# print(can_finish(5, [[1, 0], [2, 1], [4, 3], [2, 4]]))
+# print(can_finish(20, [[0, 10], [3, 18], [5, 5], [6, 11], [11, 14], [13, 1], [15, 1], [17, 4]]))
+
+assert can_finish(2, [[1, 0], [0, 1]]) is False
+assert can_finish(3, [[1, 0], [2, 1], [0, 2]]) is False
+assert can_finish(4, [[1, 0], [2, 0], [3, 1], [3, 2]]) is True
+
+# l = [[0, 10], [3, 18], [5, 5], [6, 11], [11, 14], [13, 1], [15, 1], [17, 4]]
+# l = [[1, 0], [2, 0], [3, 1], [3, 2]]
+# print([(e[1], e[0]) for e in l])
